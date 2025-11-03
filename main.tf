0b1154c78292cce6d54b554c8daabf4f1fae110d
reguramr <regudevops@gmail.com>
2025-11-04 05:26:38 +0530

    Swap AMI IDs and update availability zone

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-02b8269d5e85954ef"  #Ubuntu
-  #ami               = "ami-089e0600a8bb6d176"  #windows
+  #ami               = "ami-02b8269d5e85954ef"  #Ubuntu
+  ami               = "ami-089e0600a8bb6d176"  #windows
   instance_type     = "t3.micro"
-  availability_zone = "ap-south-1b"
-  #availability_zone = "ap-south-1a"
+  #availability_zone = "ap-south-1b"
+  availability_zone = "ap-south-1a"
   tags = {
-     "Name" = "web1"
-    #"tag1" = "Update-test-1"    
+    #"Name" = "web1"
+    "tag1" = "Update-test-1"    
   }
 }
-
